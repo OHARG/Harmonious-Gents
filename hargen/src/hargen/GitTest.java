34b427b17b3527ab0e7d6b61c922ab18f54db029
Experimental1212 <cmhurripsh@gmail.com>
2014-05-04 08:51:01 -0400

    Added simple print statements for testing changes to GitTest.java

--- hargen/src/hargen/GitTest.java
+++ hargen/src/hargen/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Git Test");
+
+        System.out.println("This will be the second");
+        System.out.println("change made to this file!");
     }
 
 }
