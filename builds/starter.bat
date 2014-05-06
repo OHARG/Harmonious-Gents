b019c30b315928bb969faf06d4f03d379b25fef1
Christopher Murphy <cmhurripsh@gmail.com>
2014-05-06 19:54:22 -0400

    Added folder for storing latest build config

---
+++ builds/starter.bat
@@ -0,0 +1,25 @@
+@echo off
+cls
+
+echo.
+echo          HARGEN PROJECT LOADER         
+echo.
+
+for /f "tokens=*" %%a in ('dir hargen /b /od') do set newest=%%a
+
+SET INPUT=hargen/%newest%
+echo =======================================
+echo ==  Load "%newest%" ?          ==
+echo ==  Press ENTER to continue or type  ==
+echo ==  a new file name (w/o extension)  ==
+echo =======================================
+echo.
+SET /P INPUT= : 
+
+:RUN
+echo loading: %INPUT%
+echo.
+java -Djava.library.path="C:\Users\MURPH\git\Harmonious-Gents\builds\hargen\lib\natives" -Xmx1024M -Xms1024M -jar %INPUT%
+
+:END
+pause
