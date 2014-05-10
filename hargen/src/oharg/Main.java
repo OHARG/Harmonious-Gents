28127b4e38ab33431df331dd12d9c4a8b600645c
Christopher Murphy <cmhurripsh@gmail.com>
2014-05-10 19:15:25 -0400

    Created Game.class, removed Main$Game.class

--- hargen/src/oharg/Main.java
+++ hargen/src/oharg/Main.java
@@ -36,23 +36,6 @@ public class Main extends BasicGame {
     private long preAverage;
     
     private String upsMessage;
-    
-    // TODO move Game class to separate file
-    public static class Game {
-        public static final String TEXTURES = "/res/textures/";
-        
-        public static boolean lock = false;
-        public static int width = 960;
-        public static int height = (int)(width * 9 / 16.0);
-        public static boolean fullScreen = false;
-        public static int ups = 200;
-        
-        public static boolean status() { return lock; }
-        
-        public static void lock() { lock = true; }
-        
-        public static void unLock() { lock = false; }
-    }
 
     public Main(String title) {
         super(title);
