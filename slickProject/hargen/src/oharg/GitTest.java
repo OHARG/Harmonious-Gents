80ebd3d94db487a20011686c670fc450c2e962a1
Christopher Murphy <cmhurripsh@gmail.com>
2014-05-11 18:17:22 -0400

    Old files -> slickProject and created Gradle
    Gradle project uses libGDX instead of Slick2d

---
+++ slickProject/hargen/src/oharg/GitTest.java
@@ -0,0 +1,12 @@
+package oharg;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+
+        System.out.println("The third change partially");
+        System.out.println("undoes the second change!");
+    }
+
+}
